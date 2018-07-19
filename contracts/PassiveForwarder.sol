d5d2db87855caef2f06dbfabdd5f6198b6f71201
Hoang Nguyen <hoang@wetrust.io>
2018-07-19 14:36:42 -0700

    passiveforwarder supports smart contract non-standard payable fallback method

--- contracts/PassiveForwarder.sol
+++ contracts/PassiveForwarder.sol
@@ -1,12 +1,17 @@
 pragma solidity ^0.4.24;
 
-/// @title Smart contract for forwarding ETH to a pre-defined recipient in the passive mode.  I.e. someone has to trigger the transfer.
+import "./ExternalCallForwarder.sol";
+
+/// @title Smart contract for forwarding ETH to a pre-defined recipient in the passive mode i.e. someone has to trigger the transfer.
+/// ExternalCallForwarder allows recipient to call any smart contracts. For example: Calling Trustcoin smart contract to transfer TRST.
 /// @author WeTrustPlatform
-contract PassiveForwarder {
+contract PassiveForwarder is ExternalCallForwarder {
+  /// @dev recipient could be a normal account or a smart contract with the payable fallback method.
+  /// @notice if recipient is a smart contract without the payable fallback method, fund will be *stuck*.
   address public recipient;
   event Received(address indexed sender, uint256 indexed value);
 
-  constructor(address _recipient) public {
+  constructor(address _recipient) ExternalCallForwarder(_recipient) public {
     recipient = _recipient;
   }
 
@@ -14,9 +19,9 @@ contract PassiveForwarder {
     emit Received(msg.sender, msg.value);
   }
 
-  function sweep() public {
-    /// @dev using .call to be compatible with non-standard smartcontract's fallback method
-    /// i.e. the targeted smart contract might have fallback method require more than 2300 gas
-    require(recipient.call.value(address(this).balance)());
+  function sweep() public returns (bool) {
+    /// @dev using .call to be compatible with non-standard payable fallback method
+    /// i.e. the target smart contract might have fallback method require more than 2300 gas
+    return recipient.call.value(address(this).balance)();
   }
 }
