cd1ebcdf13130d679909e86caf9ed2148e6f3c7f
Hoang Nguyen <hoang@wetrust.io>
2018-07-19 14:44:30 -0700

    consolidate forwarders

--- contracts/PassiveForwarder.sol
+++ contracts/PassiveForwarder.sol
@@ -1,17 +1,16 @@
 pragma solidity ^0.4.24;
 
-import "./ExternalCallForwarder.sol";
-
 /// @title Smart contract for forwarding ETH to a pre-defined recipient in the passive mode i.e. someone has to trigger the transfer.
-/// ExternalCallForwarder allows recipient to call any smart contracts. For example: Calling Trustcoin smart contract to transfer TRST.
+/// It also allows recipient to call any smart contracts. For example: Calling Trustcoin smart contract to transfer TRST.
 /// @author WeTrustPlatform
-contract PassiveForwarder is ExternalCallForwarder {
-  /// @dev recipient could be a normal account or a smart contract with the payable fallback method.
-  /// @notice if recipient is a smart contract without the payable fallback method, fund will be *stuck*.
+contract PassiveForwarder {
+  /// @dev recipient must be a normal account or a smart contract with the standard payable fallback method.
+  /// Otherwise, fund will be stuck!
   address public recipient;
+
   event Received(address indexed sender, uint256 indexed value);
 
-  constructor(address _recipient) ExternalCallForwarder(_recipient) public {
+  constructor(address _recipient) public {
     recipient = _recipient;
   }
 
@@ -19,9 +18,31 @@ contract PassiveForwarder is ExternalCallForwarder {
     emit Received(msg.sender, msg.value);
   }
 
-  function sweep() public returns (bool) {
-    /// @dev using .call to be compatible with non-standard payable fallback method
-    /// i.e. the target smart contract might have fallback method require more than 2300 gas
-    return recipient.call.value(address(this).balance)();
+  function sweep() public {
+    recipient.transfer(address(this).balance);
+  }
+
+  /// @dev Courtesy of https://github.com/gnosis/MultiSigWallet/blob/master/contracts/MultiSigWallet.sol
+  /// This method allows the pre-defined recipient to call other smart contracts.
+  function execute(address destination, uint256 value, bytes data) public returns (bool) {
+    require(msg.sender == recipient);
+    uint256 dataLength = data.length;
+    bool result;
+    assembly {
+      let x := mload(0x40)   // "Allocate" memory for output (0x40 is where "free memory" pointer is stored by convention)
+      let d := add(data, 32) // First 32 bytes are the padded length of data, so exclude that
+      result := call(
+        sub(gas, 34710),   // 34710 is the value that solidity is currently emitting
+                           // It includes callGas (700) + callVeryLow (3, to pay for SUB) + callValueTransferGas (9000) +
+                           // callNewAccountGas (25000, in case the destination address does not exist and needs creating)
+        destination,
+        value,
+        d,
+        dataLength,        // Size of the input (in bytes) - this is what fixes the padding problem
+        x,
+        0                  // Output is ignored, therefore the output size is zero
+      )
+    }
+    return result;
   }
 }
