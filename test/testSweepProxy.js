20fb7d441fccfdc7835b76219d7ee4ac1cc6123f
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-01-18 13:38:13 +0700

    Fix linter

--- test/testSweepProxy.js
+++ test/testSweepProxy.js
@@ -4,25 +4,24 @@ const Forwarder = artifacts.require('PassiveForwarder');
 contract('SweepProxy', (accounts) => {
   const recipient = accounts[1];
   const sender = accounts[2];
-  const owner = accounts[3];
+  const anybody = accounts[3];
   const amount = web3.toWei('2', 'ether');
-  const pool_size = 10;
-  
+  const poolSize = 1;
+
   it('should be able to proxy sweep', () => SweepProxy.new()
     .then(async (instance) => {
-    let forwarders = [];
-    for (i = 0; i < pool_size; i++) {
-      pf = await Forwarder.new(recipient);
-      tx = await pf.sendTransaction({
-        from: sender,
-        value: amount,
-        gas: '23000',
-      });
-      forwarders.push(pf.address);
-    }
-    return instance.sweep(forwarders, { from: owner });
-  }).then(() => web3.eth.getBalance(recipient)).then((balance) => {
-    // verify balance
-    assert.equal(balance, amount * pool_size);
-  }));
+      const forwarders = [];
+      for (let i = 0; i < poolSize; i += 1) {
+        const pf = await Forwarder.new(recipient);
+        await pf.sendTransaction({
+          from: sender,
+          value: amount,
+        });
+        forwarders.push(pf.address);
+      }
+      return instance.sweep(forwarders, { from: anybody });
+    }).then(() => web3.eth.getBalance(recipient)).then((balance) => {
+      // verify balance
+      assert.equal(balance, amount * poolSize);
+    }));
 });
